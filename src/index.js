8d5dacadb47e14ae80a8109ac18c87f95711584c
Cole Manel <csmanel@gmail.com>
2023-12-16 14:27:11 -0500

    small styling on item props

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-// import Example from './scripts/application';
-import weaponFetch, { witherhoardItem } from './scripts/application';
+import Example from './scripts/application';
+import itemFetch from './scripts/itemFetch';
 
 document.addEventListener('DOMContentLoaded', () => {
   const main = document.getElementById('main');
-  // new Example(main);
+  new Example(main);
 
   console.log('hello world');
   // this is where things will actually start
-  console.log(weaponFetch('Witherhoard'));
+  // console.log(weaponFetch('Witherhoard'));
 });
--- src/scripts/application.js
+++ src/scripts/application.js
@@ -1,3 +1,5 @@
+import 'the-new-css-reset/css/reset.css';
+
 class Example {
   constructor(ele) {
     this.ele = ele;
@@ -8,64 +10,4 @@ class Example {
     this.ele.children[0].innerText = 'slightly different working!';
   }
 }
-// export default Example;
-
-const apiKey = '3fd152d590a94ab088993fcc9292e6f9';
-const manifestUrl = 'https://www.bungie.net/Platform/Destiny2/Manifest/';
-// const jsonFilePath = './d2.json';
-
-export default async function weaponFetch(searchName) {
-  fetch(manifestUrl, {
-    headers: {
-      'X-API-Key': apiKey,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const manifest = data.Response;
-      const inventoryUrl =
-        manifest.jsonWorldComponentContentPaths.en
-          .DestinyInventoryItemDefinition;
-
-      fetch('https://www.bungie.net' + inventoryUrl, {
-        headers: {
-          'Accept-Encoding': 'gzip',
-        },
-      })
-        .then((response) => response.json())
-        .then((inventoryData) => {
-          const codeItem = Object.values(inventoryData).filter(
-            (item) => item.displayProperties.name === searchName
-          );
-          if (searchName) {
-            const {
-              displayProperties,
-              flavorText,
-              itemTypeAndTierDisplayName,
-              itemTypeDisplayName,
-            } = codeItem[0];
-            console.log(
-              'Code item found!!!!!!',
-              displayProperties,
-              flavorText,
-              itemTypeAndTierDisplayName,
-              itemTypeDisplayName
-            );
-          } else {
-            console.log('Ignition Code item not found :(((((((( ');
-          }
-        })
-        .catch((error) => {
-          console.error('Error fetching inventory data:', error);
-        });
-    });
-}
-
-const witherhoardItem = await weaponFetch('Witherhoard');
-const breakneckItem = await weaponFetch('Breakneck');
-const osteoStrigaItem = await weaponFetch('Osteo Striga');
-const gnawingHungerItem = await weaponFetch('Gnawing Hunger');
-const forbearanceItem = await weaponFetch('Forbearance');
-const cataclysmicItem = await weaponFetch('Cataclysmic');
-const retrofitEscapadeItem = await weaponFetch('Retrofit Escapade');
-const apexPredatorItem = await weaponFetch('Apex Predator');
+export default Example;
