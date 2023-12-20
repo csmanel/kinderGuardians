a79bb2c1b8e93f2791846a3a69f66c3795314231
Cole Manel <csmanel@gmail.com>
2023-12-19 21:10:02 -0500

    add modal functionality

--- src/index.js
+++ src/index.js
@@ -2,19 +2,16 @@ import Example from './scripts/application';
 import itemFetch from './scripts/itemFetch';
 import text from './styles/text.scss';
 import itemPage from './styles/itemPage.scss';
+import modalPage from './styles/modal.scss';
 
 document.addEventListener('DOMContentLoaded', () => {
   const main = document.getElementById('main');
   new Example(main);
 
-  // const modalOpen = document.getElementById('modal-open');
-  // modalOpen.addEventListener('click', function () {
-  //   console.log('opening');
-  //   modal.style.display = 'flex';
-  // });
-  const modalClose = document.getElementById('modal-close');
-  const modal = document.getElementById('modal-container');
-  modalClose.addEventListener('click', function () {
+  const modal = document.querySelector('.modal');
+  const traveler = document.getElementsByClassName('traveler');
+
+  traveler[0].addEventListener('click', function () {
     modal.style.display = 'none';
   });
 });
